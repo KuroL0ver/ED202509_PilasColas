3d1df066f535bb23fc18ec4e1fef7f0f73659172
Camila Fimbres <camifimbres@gmail.com>
2025-10-01 08:01:49 -0700

    Confirmación

--- Arreglos/Cola.cs
+++ Arreglos/Cola.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Arreglos
+﻿namespace Arreglos
 {
     internal class Cola
     {
+        int _inicio;
+        int _final;
+
+
     }
 }
